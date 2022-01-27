be52e71b6c991955044fb5f0de42283269686587
Methmi Nugawela <methmi.nuga@gmail.com>
2022-01-27 19:14:07 +0530

    Added progress bar indicator for order expiration (Management view)

--- lib/screens/management/order_management/components/order_list.dart
+++ lib/screens/management/order_management/components/order_list.dart
@@ -52,6 +52,11 @@ class _OrderListState extends State<OrderList> {
                     child: ListView.builder(
                       itemCount: snapshot.data!.length,
                       itemBuilder: (BuildContext context, int index) {
+                        Duration minutesPassed = DateTime.now().difference(
+                            DateTime.fromMillisecondsSinceEpoch(snapshot
+                                .data![index]
+                                .createdAt
+                                .millisecondsSinceEpoch));
                         return AnimatedContainer(
                           duration: Duration(milliseconds: 200),
                           width: MediaQuery.of(context).size.width,
@@ -131,62 +136,86 @@ class _OrderListState extends State<OrderList> {
                                           fontWeight: FontWeight.bold,
                                           fontSize: 13),
                                     ),
+                                    Spacer(),
+                                    minutesPassed.inMinutes <= 60.0
+                                        ? Stack(
+                                            children: [
+                                              Container(
+                                                color: Colors.grey[600],
+                                                height: 5.0,
+                                                width: MediaQuery.of(context)
+                                                        .size
+                                                        .width *
+                                                    0.7,
+                                              ),
+                                              Container(
+                                                color: AppColors.success,
+                                                height: 5.0,
+                                                width: (minutesPassed
+                                                            .inMinutes /
+                                                        Constants
+                                                            .expirationPeriod) *
+                                                    MediaQuery.of(context)
+                                                        .size
+                                                        .width *
+                                                    0.7,
+                                              )
+                                            ],
+                                          )
+                                        : SizedBox()
                                   ],
                                 ),
                                 Spacer(),
-                                // snapshot.data![index].status ==
-                                //         Constants.orderStatus[1]
-                                //     ? GestureDetector(
-                                //         onTap: () {
-                                //           showConfirmDialog(context, () async {
-                                //             dynamic res = await OrderService
-                                //                 .updateOrderStatus(
-                                //               snapshot.data![index].id,
-                                //               snapshot.data![index].productId,
-                                //               true,
-                                //               snapshot.data![index].quantity,
-                                //             );
-                                //             if (res is SuccessMessage) {
-                                //               Navigator.pop(context);
-                                //               Navigator.popAndPushNamed(context,
-                                //                   AppRoutes.ORDER_MANAGEMENT,
-                                //                   arguments: {
-                                //                     'title': 'Active Orders',
-                                //                     'status':
-                                //                         Constants.orderStatus[1]
-                                //                   });
-                                //               await showCoolAlert(
-                                //                   context, true, res.message);
-                                //             } else
-                                //               await showCoolAlert(
-                                //                   context, false, res.message);
-                                //           });
-                                //         },
-                                //         child: Container(
-                                //           decoration: BoxDecoration(
-                                //             color: Colors.greenAccent[400],
-                                //             borderRadius: BorderRadius.only(
-                                //               topRight: Radius.circular(10),
-                                //               bottomRight: Radius.circular(10),
-                                //               topLeft: Radius.circular(0),
-                                //               bottomLeft: Radius.circular(0),
-                                //             ),
-                                //           ),
-                                //           child: Padding(
-                                //             padding: const EdgeInsets.all(15.0),
-                                //             child: Center(
-                                //                 child: Icon(
-                                //               Icons.check,
-                                //               color: Colors.white,
-                                //               size: 25,
-                                //             )),
-                                //           ),
-                                //         ),
-                                //       )
-                                //     : SizedBox()
-                                OrderExpirationDisplay(
-                                    createdDateTime:
-                                        snapshot.data![index].createdAt)
+                                snapshot.data![index].status ==
+                                        Constants.orderStatus[1]
+                                    ? GestureDetector(
+                                        onTap: () {
+                                          showConfirmDialog(context, () async {
+                                            dynamic res = await OrderService
+                                                .updateOrderStatus(
+                                              snapshot.data![index].id,
+                                              snapshot.data![index].productId,
+                                              true,
+                                              snapshot.data![index].quantity,
+                                            );
+                                            if (res is SuccessMessage) {
+                                              Navigator.pop(context);
+                                              Navigator.popAndPushNamed(context,
+                                                  AppRoutes.ORDER_MANAGEMENT,
+                                                  arguments: {
+                                                    'title': 'Active Orders',
+                                                    'status':
+                                                        Constants.orderStatus[1]
+                                                  });
+                                              await showCoolAlert(
+                                                  context, true, res.message);
+                                            } else
+                                              await showCoolAlert(
+                                                  context, false, res.message);
+                                          });
+                                        },
+                                        child: Container(
+                                          decoration: BoxDecoration(
+                                            color: Colors.greenAccent[400],
+                                            borderRadius: BorderRadius.only(
+                                              topRight: Radius.circular(10),
+                                              bottomRight: Radius.circular(10),
+                                              topLeft: Radius.circular(0),
+                                              bottomLeft: Radius.circular(0),
+                                            ),
+                                          ),
+                                          child: Padding(
+                                            padding: const EdgeInsets.all(15.0),
+                                            child: Center(
+                                                child: Icon(
+                                              Icons.check,
+                                              color: Colors.white,
+                                              size: 25,
+                                            )),
+                                          ),
+                                        ),
+                                      )
+                                    : SizedBox()
                               ],
                             ),
                           ),
