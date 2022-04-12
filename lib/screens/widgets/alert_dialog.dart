2204be2e816877ffcc55a4ac2285a5770420bf38
Akalanka Perera <akalanka@parallax.lk>
2022-04-12 21:24:48 +0530

    Linked cool alert to git repository

--- lib/screens/widgets/alert_dialog.dart
+++ lib/screens/widgets/alert_dialog.dart
@@ -4,17 +4,18 @@ import 'package:sliit_eats/helpers/colors.dart';
 
 Future showCoolAlert(BuildContext context, bool success, String alertMessage, {bool noAutoClose = false}) async {
   await CoolAlert.show(
-      context: context,
-      text: alertMessage,
-      title: "",
-      autoCloseDuration: noAutoClose ? null : Duration(milliseconds: success ? 1900 : 1800),
-      backgroundColor: Colors.transparent,
-      lottieAsset: "assets/animations/modals/${success ? "success" : "error"}.json",
-      iconHeight: success ? 140 : 120,
-      loopAnimation: false,
-      type: CoolAlertType.success,
-      confirmBtnText: "Got It",
-      showButtons: noAutoClose);
+    context: context,
+    text: alertMessage,
+    title: "",
+    autoCloseDuration: noAutoClose ? null : Duration(milliseconds: success ? 1900 : 1800),
+    backgroundColor: Colors.transparent,
+    lottieAsset: "assets/animations/modals/${success ? "success" : "error"}.json",
+    iconHeight: success ? 140 : 120,
+    loopAnimation: false,
+    type: CoolAlertType.success,
+    confirmBtnText: "Got It",
+    showButtons: noAutoClose,
+  );
 }
 
 Future showConfirmDialog(BuildContext context, Function onConfirm) async {
